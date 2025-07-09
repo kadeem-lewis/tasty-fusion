643f2e9f7e49526b58b16394c3832cc3979b96bc
kadeem-lewis <email@kadeemlewis.com>
2025-07-09 02:14:42 -0400

    updated app to use v4 folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/fonts'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image", "@nuxt/fonts"],
+});
