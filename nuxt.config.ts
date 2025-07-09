06802fd4346c43cb9805e6548195b34b05264eb3
kadeem-lewis <email@kadeemlewis.com>
2025-07-09 02:36:09 -0400

    added primevue and tailwindcss to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import Lara from "@primeuix/themes/lara";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   future: {
@@ -6,5 +9,26 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image", "@nuxt/fonts"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@primevue/nuxt-module",
+  ],
+  css: ["~/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
+  primevue: {
+    options: {
+      theme: {
+        preset: Lara,
+      },
+    },
+    components: {
+      exclude: ["Form", "FormField"],
+    },
+  },
+  devServer: {
+    port: 3003,
+  },
 });
