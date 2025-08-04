0ab9a84ea817ef007e794dc78f848fcb0fdecb71
kadeem-lewis <email@kadeemlewis.com>
2025-08-04 03:03:56 -0400

    created a section for displaying business contact details

---
+++ app/components/ContactInfo.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const contactInfoItems = [
+  {
+    icon: "material-symbols:location-on",
+    title: "Address",
+    subtitle: "63 Hunter St, Albouystown, Georgetown",
+  },
+  {
+    icon: "material-symbols:call",
+    title: "Phone",
+    subtitle: "+1 (123) 456-7890",
+  },
+  {
+    icon: "material-symbols:mail",
+    title: "Email",
+    subtitle: "example@example.com",
+  },
+  {
+    icon: "material-symbols:alarm",
+    title: "Opening Hours",
+    subtitle: "Mon - Fri: 9 AM - 5 PM",
+  },
+];
+</script>
+<template>
+  <Card>
+    <template #title> Contact Information </template>
+    <template #content>
+      <div class="flex flex-col gap-4">
+        <ContactInfoItem
+          v-for="item of contactInfoItems"
+          :key="item.title"
+          v-bind="item"
+        />
+      </div>
+    </template>
+  </Card>
+</template>
---
+++ app/components/ContactInfoItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { icon, title, subtitle } = defineProps<{
+  title: string;
+  icon: string;
+  subtitle: string;
+}>();
+</script>
+<template>
+  <div class="flex gap-2 items-center">
+    <Chip>
+      <Icon :name="icon" />
+    </Chip>
+    <div>
+      <p class="font-semibold">{{ title }}</p>
+      <p class="text-sm">{{ subtitle }}</p>
+    </div>
+  </div>
+</template>
