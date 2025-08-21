57879b54d6a8495bad460b7ae776df6ec71a0639
kadeem-lewis <email@kadeemlewis.com>
2025-08-21 16:58:46 -0400

    began attempting to setup nuxt content

---
+++ content.config.ts
@@ -0,0 +1,21 @@
+import { defineContentConfig, defineCollection, z } from "@nuxt/content";
+
+export default defineContentConfig({
+  collections: {
+    menu: defineCollection({
+      type: "data",
+      source: "menu/**.json",
+      schema: z.object({
+        name: z.string(),
+        isActive: z.boolean().default(true),
+        items: z.array(
+          z.object({
+            name: z.string(),
+            description: z.string().optional(),
+            price: z.number().positive(),
+          })
+        ),
+      }),
+    }),
+  },
+});
