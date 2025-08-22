59c373873bad771fc683b372f6dcbdc5db886419
kadeem-lewis <email@kadeemlewis.com>
2025-08-22 18:53:47 -0400

    upgraded nuxt to v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  future: {
-    compatibilityVersion: 4,
-  },
   devtools: { enabled: true },
   modules: [
     "@nuxt/eslint",
