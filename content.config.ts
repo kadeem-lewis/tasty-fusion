445354d4df3e10d19d21d6ae981cc45e88d6fdc0
kadeem-lewis <email@kadeemlewis.com>
2025-08-22 01:44:47 -0400

    updated menu content fields and added new categories

--- content.config.ts
+++ content.config.ts
@@ -8,11 +8,14 @@ export default defineContentConfig({
       schema: z.object({
         name: z.string(),
         isActive: z.boolean().default(true),
+        order: z.number().int().positive(),
         items: z.array(
           z.object({
             name: z.string(),
             description: z.string().optional(),
+            isAvailable: z.boolean().default(true),
             price: z.number().positive().int(),
+            variants: z.array(z.string()).optional(),
           })
         ),
       }),
