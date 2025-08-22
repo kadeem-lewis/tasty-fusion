a842ad1cfe2f980117b090e5b5eccccb13eb5c10
kadeem-lewis <email@kadeemlewis.com>
2025-08-22 02:29:21 -0400

    updated style of topbar

--- app/components/TopBar.vue
+++ app/components/TopBar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <p>63 Hunter St, Albouystown</p>
+  <div class="py-4 bg-neutral-500">
+    <ul class="flex list-disc gap-6 items-center justify-center">
+      <li class="list-none">63 Hunter St</li>
+      <li>Albouystown, Georgetown</li>
+      <li>
+        <ULink href="tel:1234567890" external> 123.456.7890 </ULink>
+      </li>
+    </ul>
   </div>
 </template>
