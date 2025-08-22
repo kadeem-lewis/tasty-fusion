f738a2a28450f4035696c485c33a54a948f3ccc1
kadeem-lewis <email@kadeemlewis.com>
2025-08-22 01:46:40 -0400

    updated menu to show new content and use nuxtui

--- app/pages/menu.vue
+++ app/pages/menu.vue
@@ -1,32 +1,39 @@
 <script setup lang="ts">
 const { data: menu } = await useAsyncData("menu", () => {
-  return queryCollection("menu").all();
+  return queryCollection("menu")
+    .where("isActive", "=", true)
+    .order("order", "ASC")
+    .all();
 });
 </script>
 <template>
-  <Card>
-    <template #title> Menu </template>
-    <template #content>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div v-for="category in menu" :key="category.name" class="">
-          <h3 class="text-2xl font-semibold text-center font-oswald">
-            {{ category.name }}
-          </h3>
-          <ul class="flex flex-col gap-4">
-            <li
-              v-for="item in category.items"
-              :key="item.name"
-              class="flex justify-between items-center gap-4"
-            >
-              <div>
-                <h4 class="text-lg">{{ item.name }}</h4>
-                <p>{{ item.description }}</p>
-              </div>
-              <p class="self-start">${{ item.price }}</p>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <UCard>
+    <template #header>
+      <h2 class="text-4xl font-bold text-center">Menu</h2>
     </template>
-  </Card>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <section
+        v-for="category in menu"
+        :key="category.name"
+        class="border-b border-dotted px-4 py-6"
+      >
+        <h3 class="text-2xl font-semibold text-primary text-center font-oswald">
+          {{ category.name }}
+        </h3>
+        <ul class="flex flex-col gap-4">
+          <li
+            v-for="item in category.items"
+            :key="item.name"
+            class="flex justify-between items-center gap-4"
+          >
+            <div>
+              <h4 class="text-lg">{{ item.name }}</h4>
+              <p class="text-muted">{{ item.description }}</p>
+            </div>
+            <p class="self-start text-muted">${{ item.price }}</p>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </UCard>
 </template>
